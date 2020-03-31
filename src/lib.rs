e834b4e0d9e2f86a240228e373c71f9519f1da1b
Dan Muir <danthemanmuir@gmail.com>
2020-03-31 10:11:30 -0400

    Set 1 - Challenge 3

--- src/lib.rs
+++ src/lib.rs
@@ -39,6 +39,18 @@ pub mod hex {
             let hex = b"68656c6c6f20776f726c64".to_vec();
 
             assert_eq!(hex.len(), encode(s).len());
+            assert_eq!(hex, encode(s));
+
+            let s = b"Hello World";
+            let hex = b"48656c6c6f20576f726c64".to_vec();
+
+            assert_eq!(hex.len(), encode(s).len());
+            assert_eq!(hex, encode(s));
+
+            let s = b"HELLO WORLD";
+            let hex = b"48454c4c4f20574f524c44".to_vec();
+
+            assert_eq!(hex.len(), encode(s).len());
             assert_eq!(hex, encode(s))
         }
 
@@ -46,6 +58,18 @@ pub mod hex {
         fn _decode() {
             let hex = b"68656c6c6f20776f726c64";
             let s = b"hello world".to_vec();
+
+            assert_eq!(s.len(), decode(hex).len());
+            assert_eq!(s, decode(hex));
+
+            let hex = b"48656c6c6f20576f726c64";
+            let s = b"Hello World".to_vec();
+
+            assert_eq!(s.len(), decode(hex).len());
+            assert_eq!(s, decode(hex));
+
+            let hex = b"48454c4c4f20574f524c44";
+            let s = b"HELLO WORLD".to_vec();
 
             assert_eq!(s.len(), decode(hex).len());
             assert_eq!(s, decode(hex))
@@ -111,7 +135,7 @@ pub mod base64 {
 
     #[allow(dead_code)]
     pub fn decode(bytes: &[u8]) -> Vec<u8> {
-        fn debase64(byte: u8) -> u8 {
+        fn de_base64(byte: u8) -> u8 {
             [
                 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 64, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66,
                 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66, 66,
@@ -131,10 +155,8 @@ pub mod base64 {
 
         fn padding_to_zero(bytes: &[u8]) -> Vec<u8> {
             let mut bytes_mut = bytes.to_vec();
-            let mut rev_bytes = bytes.iter().rev();
-            let mut i = bytes.len();
-            while let Some(b'=') = rev_bytes.next() {
-                i -= 1;
+            let mut rev_bytes = bytes.iter().enumerate().rev();
+            while let Some((i, b'=')) = rev_bytes.next() {
                 if let Some(b) = bytes_mut.get_mut(i) {
                     *b = b'A';
                 }
@@ -145,10 +167,10 @@ pub mod base64 {
 
         fn pack_24_bit_bytes(byte_chunk: &[u8]) -> u32 {
             vec![
-                (debase64(byte_chunk[0]) as u32) << 18,
-                (debase64(byte_chunk[1]) as u32) << 12,
-                (debase64(byte_chunk[2]) as u32) << 6,
-                debase64(byte_chunk[3]) as u32,
+                (de_base64(byte_chunk[0]) as u32) << 18,
+                (de_base64(byte_chunk[1]) as u32) << 12,
+                (de_base64(byte_chunk[2]) as u32) << 6,
+                de_base64(byte_chunk[3]) as u32,
             ]
             .into_iter()
             .sum()
@@ -176,6 +198,12 @@ pub mod base64 {
 
         #[test]
         fn _encode() {
+            let base64_str = b"aGVsbG8gd29ybGQ=".to_vec();
+            let s = b"hello world";
+
+            assert_eq!(base64_str.len(), encode(s).len());
+            assert_eq!(base64_str, encode(s));
+
             let base64_str = b"aGVsbG8gd29ybGQK".to_vec();
             let s = b"hello world\n";
 
@@ -213,6 +241,96 @@ pub mod xor {
     pub fn fixed_xor(a: &[u8], b: &[u8]) -> Vec<u8> {
         a.iter().zip(b.iter()).map(|(a, b)| a ^ b).collect()
     }
+
+    #[allow(dead_code)]
+    pub fn single_byte_xor(key: u8) -> impl Fn(&[u8]) -> Vec<u8> {
+        move |slice: &[u8]| slice.iter().map(|byte| byte ^ key).collect()
+    }
+
+    #[allow(dead_code)]
+    pub fn decrypt_single_byte_xor(slice: &[u8]) -> Option<(Vec<u8>, u8)> {
+        use super::heuristics::byte_frequency;
+
+        (0..127) // Assuming the key is only within the ascii range
+            .map(|key| {
+                let decrypted_slice: Vec<u8> = single_byte_xor(key)(slice);
+                let score = byte_frequency(&decrypted_slice);
+
+                (decrypted_slice, (key, score))
+            })
+            .max_by_key(|(_slice, (_key, score))| *score)
+            .map(|(decrypted_slice, (key, _score))| (decrypted_slice, key))
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test]
+        fn _single_byte_xor() {
+            let xor = single_byte_xor(b'a');
+            let slice = b"Hello World";
+
+            let actual = xor(&xor(slice));
+
+            assert_eq!(slice.to_vec(), actual)
+        }
+    }
+}
+
+pub mod heuristics {
+    use std::collections::HashMap;
+
+    #[allow(dead_code)]
+    pub fn byte_frequency(slice: &[u8]) -> i32 {
+        let mut hit_records = HashMap::new();
+
+        slice.iter().for_each(|b| {
+            let counter = hit_records.entry(*b).or_insert(0.0);
+            *counter += 1.0;
+        });
+
+        weight_scores(hit_records)
+    }
+
+    pub fn weight_scores(hit_records: HashMap<u8, f32>) -> i32 {
+        let weights = vec![
+            (b'a', 1.08167),
+            (b'b', 1.01482),
+            (b'c', 1.02782),
+            (b'd', 1.04253),
+            (b'e', 1.12702),
+            (b'f', 1.02228),
+            (b'g', 1.02015),
+            (b'h', 1.06094),
+            (b'i', 1.06094),
+            (b'j', 1.00153),
+            (b'k', 1.00772),
+            (b'l', 1.04025),
+            (b'm', 1.02406),
+            (b'n', 1.06749),
+            (b'o', 1.07507),
+            (b'p', 1.01929),
+            (b'q', 1.00095),
+            (b'r', 1.05987),
+            (b's', 1.06327),
+            (b't', 1.09056),
+            (b'u', 1.02758),
+            (b'v', 1.00978),
+            (b'w', 1.02360),
+            (b'x', 1.00150),
+            (b'y', 1.01974),
+            (b'z', 1.00074),
+            (b' ', 1.13000),
+        ]
+        .into_iter()
+        .collect::<HashMap<u8, f32>>();
+
+        hit_records
+            .iter()
+            .map(|(byte, hits)| *weights.get(&byte.to_ascii_lowercase()).unwrap_or(&1.00) * hits)
+            .sum::<f32>() as i32
+    }
 }
 
 #[cfg(test)]
@@ -224,7 +342,10 @@ mod set1 {
 
         #[test]
         fn convert_hex_to_base64() {
-            let from_hex_bytes = hex::decode(b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+            let from_hex_bytes = hex::decode(
+                b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+            );
+
             let base64_str =
                 b"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t".to_vec();
 
@@ -236,13 +357,30 @@ mod set1 {
         use super::*;
 
         #[test]
-        fn xor_two_buffers() {
+        fn fixed_xor() {
             let a = hex::decode(b"1c0111001f010100061a024b53535009181c");
             let b = hex::decode(b"686974207468652062756c6c277320657965");
             let expected = b"746865206b696420646f6e277420706c6179".to_vec();
 
             let actual = xor::fixed_xor(&a, &b);
             assert_eq!(expected, hex::encode(&actual))
+        }
+    }
+
+    mod challenge3 {
+        use super::*;
+
+        #[test]
+        fn single_byte_xor() {
+            let slice = hex::decode(
+                b"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736",
+            );
+            let (actual, _likely_byte) = xor::decrypt_single_byte_xor(&slice).unwrap();
+
+            assert_eq!(
+                String::from("Cooking MC's like a pound of bacon"),
+                String::from_utf8(actual).unwrap()
+            )
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 mod lib;
 
 use lib::hex;
+use lib::xor;
 
 fn main() {
     println!(
         "Set 1 - Challenge 1: {}", 
         String::from_utf8(hex::decode(b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")).unwrap()
-    )
-    ;
+    );
     println!(
         "Set 1 - Challenge 2: {}",
         String::from_utf8(hex::decode(b"746865206b696420646f6e277420706c6179")).unwrap()
     );
+
+    println!(
+        "Set 1 - Challenge 3: {}",
+        String::from_utf8(
+            xor::decrypt_single_byte_xor(
+                &hex::decode(
+                    b"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736",
+                ) // decode hex code to byte slice
+            ) // decrypt single byte xor
+            .unwrap() // unwrap option
+            .0 // read decrypted slice only
+        )
+        .unwrap(),
+    )
 }
